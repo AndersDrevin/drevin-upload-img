7542216c6cda4b918adbe7d3a62ae65851a6f6dd
Anders Drevin <anders.drevin@gmail.com>
2020-02-23 15:50:05 +0100

    Nudå

--- app.js
+++ app.js
@@ -6,28 +6,35 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/image.png', function(req, res){
-    res.sendFile(path.join(__dirname, 'image.png'));
+    res.sendFile(path.join(__dirname, '/image.png'));
     //res.sendFile(__dirname + '/uploads/image.png');
 });
 
 app.post('/', function (req, res){
+
+    try{
+        console.log("LEvel 1");
     var form = new formidable.IncomingForm();
 
     form.parse(req);
 
     form.on('fileBegin', function (name, file){
-        file.path = path.join(__dirname, 'image.png');
+        file.path = path.join(__dirname, '/image.png');
     });
 
     form.on('file', function (name, file){
         console.log('Uploaded image.png');
     });
 
-    res.sendFile(path.join(__dirname, 'index.html'));
+    }catch(ex){
+        console.log("Det Sket sig", ex);
+    }
+
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
